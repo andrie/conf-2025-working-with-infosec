0e39b23466103eff606b9c97a2c0f9551eddcc42
andrie <apdevries@gmail.com>
2025-09-17 06:22:05 -0400

    Remove unused scenes

--- animations/src/scenes/hexagons.tsx
+++ animations/src/scenes/hexagons.tsx
@@ -3,36 +3,21 @@ import {Node, Circle, Polygon, Txt, Layout} from '@motion-canvas/2d/lib/componen
 import {delay, waitFor, waitUntil} from '@motion-canvas/core/lib/flow';
 import {createRef, makeRef, range, useRandom} from '@motion-canvas/core/lib/utils';
 import {all, chain, sequence} from '@motion-canvas/core/lib/flow';
+import { Colors, Posit } from '../styles';
 
-// function aLabel(ref:any, str: string, x:number, y: number){
-//   return(
-//          <Txt x={x} y={y} ref={ref} text={str} fill="white" />
-//   );
-// }
-
-// function aPoly(ref:any) {
-//   return(
-//   <Polygon 
-//     ref={ref} x={0} y={0} width={320} height={320} sides={3} 
-//     stroke={'#fff'} lineWidth={8}
-//     rotation={1 * 360/3}
-//     fill={'orange'}
-//   />
-//   )
-// }
-
-
-function gridPoly(ref:any, x:number, y:number, size:number) {
+function gridPoly(ref:any, x:number, y:number, size:number, opacity:number) {
   return(
     <Polygon 
       ref={ref}
       x={x}
       y={y}
       size={size}
-      fill="#eef"
+      fill={Posit.gray}
       lineWidth={3}
-      stroke={'white'}
+      // stroke={Colors.whiteAlpha50}
+      stroke={Colors.almostBlack}
       scale={0}
+      opacity={opacity}
     />
   )
 }
@@ -47,7 +32,7 @@ export default makeScene2D(function* (view) {
   const nrows = 9;
   const ncols = 14;
   const highlight = 25; // the polygon to highlight
-  const highlightColor = '#f00';
+  const highlightColor = Posit.blue;
 
   view.add(
   <>
@@ -56,23 +41,25 @@ export default makeScene2D(function* (view) {
         gridPoly(makeRef(polys, i * ncols + j), 
         (j - (ncols / 2)) * size * cos(30) + (i % 2) * size * cos(30) / 2, 
         -size * i * 1.5 * sin(30) + (nrows + 2) * size * sin(30) / 2, 
-        size)
-      )
-    )}
+        size,
+        (1 - i / nrows) * 0.5
+    )))
+    }
   </>
   );
 
   yield* all(
-    sequence( 0.01, ...polys.map(poly => poly.scale(1, 2))),
+    sequence( 0.01, ...polys.map((poly, index) => poly.scale(1, 2))),
     chain(
       waitFor(1),
-      sequence( 0.01, ...polys.map(poly => poly.fill('lightblue', 2))),
+      sequence( 0.01, ...polys.map((poly, index) => {
+        const opacity = (1 - index / polys.length) * 0.8;
+        return all(
+          poly.fill(Posit.teal, 2),
+          poly.opacity(opacity, 2)
+        );
+      })),
       ),
-    chain(
-      waitFor(2),
-      polys[highlight].fill(highlightColor, 1),
-      waitFor(1)
-    )
 
   );
     
