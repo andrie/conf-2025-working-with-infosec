f979d3131b18be53b83967f27b0793d4fb84a880
andrie <apdevries@gmail.com>
2025-10-08 00:23:00 +0100

    Add appBox pulse

--- _extensions/andrie/motion-canvas/motion-canvas-player.js
+++ _extensions/andrie/motion-canvas/motion-canvas-player.js
@@ -10,17 +10,20 @@ function stopAnimation(event) {
     // TODO: make it work with multiple animations per slide
     
     // start the animation
-    console.log('setting auto to true')
+    console.log('motion-canvas.lua: setting auto to true')
+    // console.log('motion-canvas.lua: loop = ', player.player.playerState("loop"))
     player.setAttribute('auto', 'true');
 
 
     const startCheckInterval = setInterval(() => {
       // wait for player to start
-      console.log('checking player...')
+      console.log('motion-canvas.lua: checking player...')
       if (player.player) {
         clearInterval(startCheckInterval);
-        const fps = 60;
+        const fps = 60; // frames per second
+        maxf = 0;
         if (player.getAttribute('loop') === 'false'  ) {
+          console.log('motion-canvas.lua: loop is false')
           const frameCheckInterval = setInterval(() => {
             if (player.player && !player.player.active) {
               player.player.activate()
@@ -28,13 +31,14 @@ function stopAnimation(event) {
             if (player.player && player.player.frame) {
               const f = player.player.frame.value;
               const nf = player.player.endFrame;
-              if (f === nf) {
+              if (f === nf || f < maxf) {
                 clearInterval(frameCheckInterval);
                 player.player.requestSeek(nf);
                 player.player.deactivate();
               }
+              maxf = f;
             }
-          }, 1000 / fps); // Check every frame (assuming 60 FPS)
+          }, 1000 / fps); // Check every frame
           
           // Clear the interval when the slide changes
           Reveal.addEventListener('slidechanged', () => {
@@ -58,11 +62,10 @@ function stopAnimation(event) {
 
 // Check every 100ms if Reveal is defined
 document.addEventListener("DOMContentLoaded", function() {
-  // Your function goes here
   const checkReveal = setInterval(function() {
     if (window.Reveal && Reveal.isReady()) {
       // If Reveal is defined, set up the event listener and clear the interval
-      console.log("Reveal is ready")
+      // console.log("Reveal is ready")
       stopAnimation()
       Reveal.on('slidechanged', stopAnimation);
       clearInterval(checkReveal);
